9d422bbdfbdbe7047dc9a68aee6caa25338a501d
varshad14 <114212179+varshad14@users.noreply.github.com>
2022-10-06 23:01:33 +0530

    git commit -m "Made changes"

--- avg.rb
+++ avg.rb
@@ -3,14 +3,11 @@
 named 'Average'having a method to calculate and print the average
 =end
 
-class Average 
-    def from_user
-        print("Enter a num1:")               #input from user
-        @num1=gets.to_i                      #using instance variables and that                                                                              converting string to integer
-        print("Enter a num2:")
-        @num2=gets.to_i
-        print("Enter a num3:")
-        @num3=gets.to_i
+class Average num1,num2,num3
+    def initialize 
+        @num1=num1                    #using instance variables and that                                                                              converting string to integer
+        @num2=num2
+        @num3=num3
     end 
     
     def calculate                             #first method for calculation
@@ -19,6 +16,5 @@ class Average
     end
    end
 
- a=Average.new                        #instance
-    a.from_user                  #calling member functions
+ a=Average.new(3,4,5)                      #instance            
     a.calculate
