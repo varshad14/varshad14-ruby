fba7747d5b6eed051a0758bf88fc0cb731d43d45
varshad14 <114212179+varshad14@users.noreply.github.com>
2022-10-11 21:05:04 +0530

    Create circle.rb

---
+++ circle.rb
@@ -0,0 +1,23 @@
+=begin Create a class  circle class with  radius as data member. Create two constructors  (noargument, and two arguments) and a method 
+  to calculate Circumference.
+=end
+  
+module CircleCircumference
+    @@PI=3.14
+        def calculate                            
+            @c= 2 * @@PI.to_i * @radius.to_i 
+            print "Circumference is #{@c}"
+        end 
+end
+
+class Circle
+    include CircleCircumference               
+    
+    def initialize radius
+       @radius=radius
+    end
+    
+   
+end
+
+Circle.new(2).calculate        
