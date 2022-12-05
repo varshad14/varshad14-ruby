3432b508dd7cce18165f9b266459687f28f58233
varshad14 <114212179+varshad14@users.noreply.github.com>
2022-12-05 22:51:20 +0530

    Adding files

--- employee.rb
+++ employee.rb
@@ -4,24 +4,28 @@
 2   -   'AddSal()'   which   adds   $10   to   salary   of   the   employee   if   it   is   less   than   $500.
 3 - 'AddWork()' which adds $5 to salary of employee if the number of hours of work perday is more than 6 hours
 =end
-class Employee                       #classname
-                          
-    def initialize  salary,hrs  
-        @salary=salary
-        @hrs=hrs
-    end
-    
-    def add_sal                #2nd method to add bonus if salary is less than  500
+module Salary
+    def add_sal                
         if  @salary.to_i< 500             #condition to check if salary is less than 500
                @bonus1=+ 10                  #if true then bonus will increase by 10
         end
-    end
-    
+    end 
+end
+module Work 
     def add_work                 #3rd method to add bonus if work time is more than  6
         if @hrs.to_i> 6        #condition to check if work time is more than 5
                 @bonus2=+5              #if true then bonus will increase by 5
        end
     end
+end
+
+class Employee                       #classname
+    include Salary, Work                      
+    def initialize  salary,hrs  
+        @salary=salary
+        @hrs=hrs
+    end
+    
 
     def final_sal
        final=@salary.to_i+@bonus1.to_i+@bonus2.to_i
@@ -32,4 +36,4 @@ end
 e1=Employee.new 200, 8
 e1.add_sal
 e1.add_work
-e1.final_sal            
+e1.final_sal 
